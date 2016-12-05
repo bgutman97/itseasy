db443bb2bbd171b647fd171a1e231c1a37cd02e6
aaroncharous <aaroncharous@gmail.com>
2016-12-04 22:57:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = "dude";
+  botResponse = "/dude";
 
   options = {
     hostname: 'api.groupme.com',
